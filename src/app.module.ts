8ff59e704161a00aedf8dcf2c29c592c125ed6f2
piiivot <the_pivotsk8@hotmail.com>
2023-11-21 11:49:02 -0500

    feat configuration mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { join } from 'path'; //Node los paquetes de node generalmente van al inicio
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
@@ -9,6 +11,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule
   ],
 })
